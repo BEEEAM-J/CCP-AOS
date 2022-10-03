e8bfb6c6645f826f3c02bca36e1df68609b97402
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2022-10-03 14:50:54 +0900

    최종

--- app/src/main/java/com/example/ccp_aos/MainActivity.kt
+++ app/src/main/java/com/example/ccp_aos/MainActivity.kt
@@ -20,7 +20,9 @@ class MainActivity : AppCompatActivity() {
 
 //  바인딩을 전역 변수로 선언 -> 다른 클래스에서도 사용할 수 있게 하기 위해서
     private  lateinit var binding: ActivityMainBinding
-    private  lateinit var category_pos : String
+
+//    lateinit var 로 선언하면 nullable이 안되서 그냥 var로 선언
+    private var category_pos : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +46,11 @@ class MainActivity : AppCompatActivity() {
 //      생성된 레트로핏 객체 사용하여 데이터 받아오기
         retrofitService.get_categories().enqueue(object : Callback<categoryList>{
             override fun onResponse(call: Call<categoryList>, response: Response<categoryList>) {
+//              카테고리 리스트 받아오기
                 cateList.addAll(response.body() as List<categoryList>)
 
 //              스피너 어댑터 선언
+//              레트로핏은 비동기라서 어댑터를 레트로핏 응답이 오고, 리스트를 받아온 다음에 어댑터를 선언해줘야함
                 var adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, cateList)
                 binding.category.adapter = adapter
 
@@ -62,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<categoryList>, t: Throwable) {
-
+                binding.content.setText("통신 실패")
             }
 
         })
@@ -75,6 +79,7 @@ class MainActivity : AppCompatActivity() {
         binding.category.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
+                // 선택된 카테고리를 String으로 받아옴
                 category_pos = cateList[p2] as String
 
             }
@@ -85,36 +90,24 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+//      이미지 버튼이 눌리면 카테고리에 맞는 농담을 출력해야함
         binding.check.setOnClickListener(){
 //            random이 선택되었을 때 Query를 null 처리
             if (category_pos == "random"){
-                retrofitService.get_jokes(null).enqueue(object : Callback<jokes>{
-                    override fun onResponse(call: Call<jokes>, response: Response<jokes>) {
-
-                        binding.content.setText("${response.body()?.value ?: String}")
-
-                    }
-
-                    override fun onFailure(call: Call<jokes>, t: Throwable) {
-                        binding.content.setText("통신 실패")
-                    }
-
-                })
+                category_pos = null
             }
-            else{
-                retrofitService.get_jokes(category_pos).enqueue(object : Callback<jokes>{
-                    override fun onResponse(call: Call<jokes>, response: Response<jokes>) {
 
-                        binding.content.setText("${response.body()?.value ?: String}")
+            retrofitService.get_jokes(category_pos).enqueue(object : Callback<jokes>{
+                override fun onResponse(call: Call<jokes>, response: Response<jokes>) {
 
-                    }
+                    binding.content.setText("${response.body()?.value ?: String}")
+                }
 
-                    override fun onFailure(call: Call<jokes>, t: Throwable) {
-                        binding.content.setText("통신 실패")
-                    }
+                override fun onFailure(call: Call<jokes>, t: Throwable) {
+                    binding.content.setText("통신 실패")
+                }
 
-                })
-            }
+            })
         }
 
 
